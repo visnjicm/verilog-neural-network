43251d6410ffbdf4b1b3e993dcaa1d6f5562975f
visnjicm <126916558+visnjicm@users.noreply.github.com>
2023-03-18 21:29:11 -0500

    Update register.sv
    Removed the assign statement, because it prevents synthesis.

--- register.sv
+++ register.sv
@@ -9,7 +9,7 @@ module register #(parameter WIDTH, BITS)
   
   always @(counter) begin
   //$display("counter_reg:%h and value_reg:%h", counter, value);
-    assign value = data[counter];
+    value = data[counter];
   //always @(counter) begin
     //$display("%d", data[index]);
   //end
